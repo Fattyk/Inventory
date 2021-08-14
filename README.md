7fb233b3c13bbe6d0e0faa8fc17b371d798eab63
Fattyk <ogundele.fatai.k@gmail.com>
2021-08-14 11:47:14 +0100

    change_password now working

--- README.md
+++ README.md
@@ -1,21 +1,94 @@
-# Inventory
+# Inventory Documentation
 
 
 ### Requirements:
 After installing Django, please run *pip install -r requirements.txt* on your command line to install rest_framework.
 
 
+**Using the App:**
+
+User must first register
+User must login to access the app
 
 
-## User Management:
-
-### - 	user must be able to register
-### -	user must be able to update profile
-### -	user must be able to retrieve his profile information
-### -	user must be able to change password
+### User Management:
 
 
-### -	user must be able to login
+#### - 	user must be able to register
+*POST /user/register/*
+
+**Description:** Registration of User
+
+```json
+HTTP 200 OK
+Allow: POST, OPTIONS
+Content-Type: application/json
+Vary: Accept
+
+{
+    "name": "Users Registration",
+    "description": "Registration of User",
+    "renders": [
+        "application/json",
+        "text/html"
+    ],
+    "parses": [
+        "application/json",
+        "application/x-www-form-urlencoded",
+        "multipart/form-data"
+    ]
+}
+
+```
+
+Content:
+
+```json
+{
+    "username" : "", // Required
+    "password" : "", // Required
+    "first_name" : "",
+    "middle_name" : "",
+    "last_name" : "",
+    "email" : "",
+    "mobile" : "",
+    "birth_date" : ""
+}
+```
+
+Suceess: status(ok)
+
+```json
+{
+    "username" : "data", 
+    "password" : "data", 
+    "first_name" : "data",
+    "middle_name" : "data",
+    "last_name" : "data",
+    "email" : "data",
+    "password" : "data",
+    "mobile" : "data",
+    "birth_date" : "data"
+}
+
+```
+
+Wrong input: status (404)
+
+```json
+{
+    "error":"Invalid Credentials"
+}
+```
+
+
+
+#### -	user must be able to update profile
+#### -	user must be able to retrieve his profile information
+#### -	user must be able to change password
+
+
+#### -	user must be able to login
 */user/login*
 ```json
 {
@@ -25,17 +98,19 @@ After installing Django, please run *pip install -r requirements.txt* on your co
 ```
 
 
-### -	user must be able to logout
+#### -	user must be able to logout
 
-## Core Functionalities: User must be able to 
 
-### -	create item (with unique code)
-### -	delete item
-### -	search for item
-### -	view search history
-### -	set item quantity upon creation
-### -	add or remove from quantity based on purchase or use
-### -	view item inventory history
-### -	can filter items based on quantity
-### -	can filter item inventory history by date.
+
+### Core Functionalities: User must be able to 
+
+#### -	create item (with unique code)
+#### -	delete item
+#### -	search for item
+#### -	view search history
+#### -	set item quantity upon creation
+#### -	add or remove from quantity based on purchase or use
+#### -	view item inventory history
+#### -	can filter items based on quantity
+#### -	can filter item inventory history by date.
 
--- users/views.py
+++ users/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.http import request
 from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from users import serializers
 from users.models import User
-from users.serializers import RegistrationSerializer, PasswordSerializer, UserDetailSerializer
+from users.serializers import RegistrationSerializer, UserDetailSerializer
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 
 # Create your views here.
 
@@ -50,25 +52,49 @@ def logout_view(request):
     return Response(status=status.HTTP_200_OK)
 
 
+@method_decorator(login_required, name='dispatch')
 class UserDetails(generics.RetrieveUpdateAPIView):
-    """Retrieve and Update Profile Information"""
+    """Retrieve and Update Profile Information of the User and not of others"""
     queryset  = User.objects.all()
     serializer_class = UserDetailSerializer
 
+    def get_queryset(self):
+        """Ensure that user cannot get or edit other users' info"""
+        qs = super().get_queryset() 
+        return qs.filter(pk=self.request.user.pk)
 
-# def ChangePassword(request):
-#     """Update User's password"""
 
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         email = request.POST['email']
-#         password = request.POST['password']
-#         confirm_password = request.POST['confirm_password']
-#         if password != confirm_password:
-#             return Response({'error':'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
-#         user = authenticate(request, username=username, password=password)
-#         if user is not None:
-#             login(request, user)
-#             return Response(status=status.HTTP_200_OK)
-#         return Response({'error':'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
-#     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)    
+@api_view(['POST'])
+@login_required
+def change_password(request):
+    """Update User's password"""
+
+    if request.method == 'POST':
+        # get the requirements to change password
+        try:
+            username = request.data['username']
+            password = request.data['password']
+            new_password = request.data['new_password']
+            confirm_password = request.data['confirm_password']
+        except:
+            return Response({'error':'Wrong input'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # get the user from database
+        user_to_change = authenticate(username=username, password=password)
+        if not user_to_change:
+            return Response({'error':'Wrong username or password'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        # check if the user match the request.user
+        check = request.user == user_to_change
+        if not check:
+            return Response({'error':'Wrong username or password'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # check that new pasword and confirm_password are thesame
+        if new_password != confirm_password:
+            return Response({'error':'Password not match'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Change the password
+        user_to_change.set_password(new_password)
+        user_to_change.save()
+        return Response({'message':'Password changed successfully'}, status=status.HTTP_200_OK)
